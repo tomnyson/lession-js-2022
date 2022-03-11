8f90d06e4ba8d9043407b28b46f3db017656c75a
tomnyson <tabletkindfire@gmail.com>
2022-03-11 11:06:25 +0700

    git add

--- lession/ca1/buoi3/js/script.js
+++ lession/ca1/buoi3/js/script.js
@@ -2,8 +2,14 @@
 function tinhTong() {
   let soThu1 = document.getElementById('sothu1').value;
   let soThu2 = document.getElementById('sothu2').value;
-  let ketqua = Number(soThu1) + Number(soThu2);
+    let soSanh = soThu1 == soThu2
+    let soSanh1 = soThu1 === soThu2
+  // let ketqua = Number(soThu1) + Number(soThu2);
+  // == === != !== > >= < <= < <=
   let elmKetqua = document.getElementById('ketqua')
-  elmKetqua.innerHTML = "result: " + ketqua
-  console.log('tinh tong',ketqua)
+  elmKetqua.innerHTML = "== " + soSanh + " <br> ===" + soSanh1
+  // console.log('tinh tong',ketqua)
 }
+
+let sosanh = !((1<0) && (2>5))
+console.log('sosanh',sosanh)
