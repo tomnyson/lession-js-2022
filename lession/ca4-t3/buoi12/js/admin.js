5ca4751a1093b415b2eb25112c23c106e66a3441
tomnyson <tabletkindfire@gmail.com>
2022-04-07 17:29:58 +0700

    java danh sach va chi tiet  buoi 13

---
+++ lession/ca4-t3/buoi12/js/admin.js
@@ -0,0 +1,22 @@
+ 
+window.addEventListener('DOMContentLoaded', function(e){
+  console.log('go here')
+ const userLogin = store.parseUser()
+   if(userLogin) {
+     if(userLogin.role === 'admin') {
+  document.getElementById('username').innerHTML =`Username: ${userLogin.username}`       // admin page
+     document.getElementById('name').innerHTML =`Username: ${userLogin.name}`       // admin page
+    
+    document.getElementById('email').innerHTML =`email: ${userLogin.name}`
+    document.getElementById('password').innerHTML =`password: ******`
+    document.getElementById('role').innerHTML =`role: ${userLogin.role}  `
+  }
+   
+   } else {
+     window.location.href = './login.html'
+   }
+   /**
+    * trường hợp user -> user page
+    * trường hợp admin -> admin page
+    */
+})
--- lession/ca4-t3/buoi12/js/script.js
+++ lession/ca4-t3/buoi12/js/script.js
@@ -1,79 +1,4 @@
-class User {
-  constructor(name, username, password, email, role){
-    this.name = name
-    this.username = username
-    this.password = password
-    this.email = email
-    this.role = role
-  }
-  getUserName() {
-    return this.username
-  }
-  getPassword() {
-    return this.password
-  }
-  xuatThongTin() {
-    console.log(`Name: ${this.name} UserName: ${this.username} password: ${this.password}
-    email: ${this.email} role: ${this.role}`)
-  }
-}
 
-class StoreUser {
-  constructor(){
-    this.users = []
-  }
-  addUser(user){
-    /**
-     * kt trung username
-     */
-    for(let i=0;i<this.users.length;i++){
-      if(this.users[i].getUserName() === user.getUserName()){
-        return false
-      }
-    }
-    this.users.push(user)
-    return true
-  }
-  login(username, password){
-    /**
-     * kt trung username
-     */
-    for(let i=0;i<this.users.length;i++){
-      if(this.users[i].getUserName() === username &&
-       this.users[i].getPassword() === password){
-        return true
-      }
-    }
-    return false
-  }
-  getListUser(){
-    return this.users
-  }
-  save() {
-    if(this.users.length >0) {
-      const convertArrayToObj = JSON.stringify(this.users)
-      localStorage.setItem('users', convertArrayToObj)
-    }
-  }
-  getData() {
-    const data = localStorage.getItem('users')
-    if(data) {
-       const arrUser = JSON.parse(data)
-       const listUser = []
-       for(let i=0;i<arrUser.length;i++){
-         const userTemp = new User(arrUser[i].name, arrUser[i].username, arrUser[i].password, arrUser[i].email, arrUser[i].role)
-         listUser.push(userTemp);
-       }
-       this.users = listUser
-    }
-  }
-
-}
-// handle UI
-const store = new StoreUser()
-// lấy data từ localstorage
-store.getData()
-console.log(store.getListUser())
 
 document.getElementById('frmDangKy')&& document.getElementById('frmDangKy').addEventListener('submit', function(event){
   event.preventDefault();
@@ -108,9 +33,25 @@ document.getElementById('frmDangNhap') && document.getElementById('frmDangNhap')
     return
   }
   const isLogin = store.login(username, password)
+  store.saveUser(isLogin)
   if(isLogin){
-    alert('đăng nhập thành công')
+    if(isLogin.getRole() === 'admin') {
+      // admin page
+    window.location.href = './admin.html'
+      alert('admin login')
+    }
+   
+    /**
+     * trường hợp user -> user page
+     * trường hợp admin -> admin page
+     */
   } else {
     alert('đăng nhập thất bại')
   }
 })
+
+/**
+ * xư lý dashboard
+ *  */
+
+ 
